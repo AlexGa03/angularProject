e2bae49de6e0a542dd6ea3594c2e43dd46377278
Alex García Álvarez <alexgarciaalvarez93@gmail.com>
2023-02-02 20:12:44 +0100

    testing angular directives

--- myApp/src/app/body/body/body.component.ts
+++ myApp/src/app/body/body/body.component.ts
@@ -8,14 +8,22 @@ import { Component } from '@angular/core';
 export class BodyComponent {
   title = 'Alexgaralv';
   personaAlex = p1;
+  deportistas: String[] = ["Nadal","Messi","Federer","Pique","Cristiano"];
+  redClass = "text-danger";
+  greenClass = "text-success";
+  clickN:number = 0;
+  changeClass(){
+    this.clickN = this.clickN+1;
+  }
 }
-export interface persona{
+interface persona{
   id:number,
   name:string,
   surname:string
 
 }
-export const p1:persona = {
+
+const p1:persona = {
   id : 1,
   name : "Alex",
   surname :"Garcia"
