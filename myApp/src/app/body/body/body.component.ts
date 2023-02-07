b0722d0b893a220d9b24cf8f83c6bf4e1e7b0443
Alex García Álvarez <alexgarciaalvarez93@gmail.com>
2023-02-07 17:28:21 +0100

    ngClass ngStyle class
    new methods to angular proyects

--- myApp/src/app/body/body/body.component.ts
+++ myApp/src/app/body/body/body.component.ts
@@ -11,10 +11,33 @@ export class BodyComponent {
   deportistas: String[] = ["Nadal","Messi","Federer","Pique","Cristiano"];
   redClass = "text-danger";
   greenClass = "text-success";
+  colorClass:string = "text-danger";
+  changeClass2(){
+    if(this.colorClass == "text-danger"){
+      this.colorClass = "text-success";
+    }else{
+      this.colorClass = "text-danger";
+    }
+  }
+  bool1 = false;
+  changeBoolClass(){
+    this.bool1 = !this.bool1;
+  }
   clickN:number = 0;
   changeClass(){
     this.clickN = this.clickN+1;
   }
+
+  objectsInSameClass={
+    'badge': true,
+    'badge-pill':true,
+    'badge-primary':true,
+    'text-danger':true,
+  };
+  stylesInTag={
+    'color':"blue",
+    'Font-weight':"bold"
+  };
 }
 interface persona{
   id:number,
