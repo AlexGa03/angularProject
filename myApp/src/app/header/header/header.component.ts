9e545442cf67f4c0b5dc18a845f4c99862f48cba
Alex García Álvarez <alexgarciaalvarez93@gmail.com>
2023-02-09 20:14:14 +0100

    header
    header  colors end

--- myApp/src/app/header/header/header.component.ts
+++ myApp/src/app/header/header/header.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  colorClass:string = 'bg-primary main';
+  changeClass(event:any){
+    var target = event.target || event.srcElement || event.currentTarget;
+    var idAttr = target.attributes.id;
+    var value = idAttr.nodeValue;
+    
+    if(value === 'blue'){
+      this.colorClass = 'bg-primary main';
+    }else if(value === 'dark'){
+      this.colorClass = 'bg-dark main text-light';
+    }else{
+      this.colorClass = 'bg-white main';
+    }
+  }
 }
